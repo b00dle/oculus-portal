7e05c3014a8c3c45275645de946bc953fc179060
Ephraim Schott <senu8384@boreas.(none)>
2014-03-13 13:31:06 +0100

    added color to rooms

--- lib/game_lib/LightRoom.py
+++ lib/game_lib/LightRoom.py
@@ -13,11 +13,13 @@ class LightRoom(avango.script.Script):
     self.NAME = "default"
     self.SCENEGRAPH = avango.gua.nodes.SceneGraph()
     self.POSITION = avango.gua.nodes.TransformNode()
+    self.COLOR = "White"
 
-  def my_constructor(self,NAME,SCENEGRAPH,POSITION, EXITS, ACTIV):
+  def my_constructor(self,NAME,SCENEGRAPH,POSITION, EXITS, ACTIV, COLOR):
     self.NAME = NAME
     self.SCENEGRAPH = SCENEGRAPH
     self.POSITION = POSITION
+    self.COLOR = COLOR
 
     loader = avango.gua.nodes.GeometryLoader()
 
@@ -25,7 +27,7 @@ class LightRoom(avango.script.Script):
     room_transform.Transform.value = self.POSITION
     self.SCENEGRAPH.Root.value.Children.value.append(room_transform)
 
-    plane = loader.create_geometry_from_file('room_floor', 'data/objects/plane.obj', 'Stone', avango.gua.LoaderFlags.DEFAULTS)
+    plane = loader.create_geometry_from_file('room_floor', 'data/objects/plane.obj', "Stone" , avango.gua.LoaderFlags.DEFAULTS)
     plane.Transform.value = avango.gua.make_scale_mat(2.4,1,2)
     plane.GroupNames.value.append("do_not_display_group")
     room_transform.Children.value.append(plane)
@@ -43,20 +45,20 @@ class LightRoom(avango.script.Script):
     top_left_upper_cornor   = avango.gua.Vec3( -1.2, 2.7, -1)
     top_left_bottom_cornor  = avango.gua.Vec3( -1.2, 2.7, 1)
 
-    create_line_visualization2(loader, room_transform, right_upper_cornor, right_bottom_cornor, 'White', 0.01)
-    create_line_visualization2(loader, room_transform, right_bottom_cornor, left_bottom_cornor, 'White', 0.01)
-    create_line_visualization2(loader, room_transform, left_bottom_cornor, left_upper_cornor, 'White', 0.01)
-    create_line_visualization2(loader, room_transform, left_upper_cornor, right_upper_cornor, 'White', 0.01)
+    create_line_visualization2(loader, room_transform, right_upper_cornor, right_bottom_cornor, self.COLOR, 0.01)
+    create_line_visualization2(loader, room_transform, right_bottom_cornor, left_bottom_cornor, self.COLOR, 0.01)
+    create_line_visualization2(loader, room_transform, left_bottom_cornor, left_upper_cornor, self.COLOR, 0.01)
+    create_line_visualization2(loader, room_transform, left_upper_cornor, right_upper_cornor, self.COLOR, 0.01)
 
-    create_line_visualization2(loader, room_transform, top_right_upper_cornor, top_right_bottom_cornor, 'White', 0.01)
-    create_line_visualization2(loader, room_transform, top_right_bottom_cornor, top_left_bottom_cornor, 'White', 0.01)
-    create_line_visualization2(loader, room_transform, top_left_bottom_cornor, top_left_upper_cornor, 'White', 0.01)
-    create_line_visualization2(loader, room_transform, top_left_upper_cornor, top_right_upper_cornor, 'White', 0.01)
+    create_line_visualization2(loader, room_transform, top_right_upper_cornor, top_right_bottom_cornor, self.COLOR, 0.01)
+    create_line_visualization2(loader, room_transform, top_right_bottom_cornor, top_left_bottom_cornor, self.COLOR, 0.01)
+    create_line_visualization2(loader, room_transform, top_left_bottom_cornor, top_left_upper_cornor, self.COLOR, 0.01)
+    create_line_visualization2(loader, room_transform, top_left_upper_cornor, top_right_upper_cornor, self.COLOR, 0.01)
 
-    create_line_visualization2(loader, room_transform, right_upper_cornor, top_right_upper_cornor, 'White', 0.01)
-    create_line_visualization2(loader, room_transform, right_bottom_cornor, top_right_bottom_cornor, 'White', 0.01)
-    create_line_visualization2(loader, room_transform, left_bottom_cornor, top_left_bottom_cornor, 'White', 0.01)
-    create_line_visualization2(loader, room_transform, left_upper_cornor, top_left_upper_cornor, 'White', 0.01)
+    create_line_visualization2(loader, room_transform, right_upper_cornor, top_right_upper_cornor, self.COLOR, 0.01)
+    create_line_visualization2(loader, room_transform, right_bottom_cornor, top_right_bottom_cornor, self.COLOR, 0.01)
+    create_line_visualization2(loader, room_transform, left_bottom_cornor, top_left_bottom_cornor, self.COLOR, 0.01)
+    create_line_visualization2(loader, room_transform, left_upper_cornor, top_left_upper_cornor, self.COLOR, 0.01)
 
 
 
